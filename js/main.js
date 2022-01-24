5e983701caf5b0b648b149b51a140ea8e01b79ea
Daniel-Sogbey <mathematics06physics@gmail.com>
2022-01-24 18:58:55 +0000

    init --continues update

---
+++ js/main.js
@@ -0,0 +1,84 @@
+const about = document.querySelector("#about");
+const contact = document.querySelector("#contact");
+const services = document.querySelector("#services");
+const openSource = document.querySelector("#open-source");
+const aboutContent = document.querySelector("#about-content");
+const contactContent = document.querySelector("#contact-content");
+const servicesContent = document.querySelector("#services-content");
+const openSourceContent = document.querySelector("#open-source-content");
+
+about.addEventListener("click", () => {
+	const aboutBox = new WinBox({
+		title: "About Me",
+		width: "300px",
+		height: "400px",
+		top: 50,
+		right: 10,
+		bottom: 50,
+		left: 10,
+		mount: aboutContent,
+		onfocus: function() {
+			this.setBackground("#00ff00");
+		},
+		onblur: function() {
+			this.setBackground("#777");
+		}
+	});
+});
+
+contact.addEventListener("click", () => {
+	const contactBox = new WinBox({
+		title: "Contact Me",
+		width: "300px",
+		height: "300px",
+		top: 50,
+		right: 10,
+		bottom: 50,
+		left: 10,
+		mount: contactContent,
+		onfocus: function() {
+			this.setBackground("#00ff00");
+		},
+		onblur: function() {
+			this.setBackground("#777");
+		}
+	});
+});
+
+services.addEventListener("click", () => {
+	const servicesBox = new WinBox({
+		title: "Services",
+		width: "300px",
+		height: "400px",
+		top: 50,
+		right: 10,
+		bottom: 50,
+		left: 10,
+		mount: servicesContent,
+		onfocus: function() {
+			this.setBackground("#00ff00");
+		},
+		onblur: function() {
+			this.setBackground("#777");
+		}
+	});
+});
+
+openSource.addEventListener("click", () => {
+	const openSourceBox = new WinBox({
+		title: "Open Source Projects",
+		width: "300px",
+		height: "400px",
+		top: 50,
+		right: 10,
+		bottom: 50,
+		left: 10,
+		mount: openSourceContent,
+		onfocus: function() {
+			this.setBackground("#00ff00");
+		},
+		onblur: function() {
+			this.setBackground("#777");
+		}
+	});
+});
