b4c9c885672d228e1505dad5c4bd2c8bd755fe4f
Daniel-Sogbey <mathematics06physics@gmail.com>
2022-01-28 12:52:09 +0000

    donate form and button added

--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,10 @@
 const about = document.querySelector("#about");
+const donate = document.querySelector("#donate-btn");
 const contact = document.querySelector("#contact");
 const services = document.querySelector("#services");
 const openSource = document.querySelector("#open-source");
+
+const formContent = document.querySelector("#form-content");
 const aboutContent = document.querySelector("#about-content");
 const contactContent = document.querySelector("#contact-content");
 const servicesContent = document.querySelector("#services-content");
@@ -74,6 +77,25 @@ openSource.addEventListener("click", () => {
 		bottom: 50,
 		left: 10,
 		mount: openSourceContent,
+		onfocus: function() {
+			this.setBackground("#00ff00");
+		},
+		onblur: function() {
+			this.setBackground("#777");
+		}
+	});
+});
+
+donate.addEventListener("click", () => {
+	const donateBox = new WinBox({
+		title: "Buy me a cup of coffee",
+		width: "300px",
+		height: "400px",
+		top: 50,
+		right: 10,
+		bottom: 50,
+		left: 10,
+		mount: formContent,
 		onfocus: function() {
 			this.setBackground("#00ff00");
 		},
