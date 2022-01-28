724ac3793385fca025a6eb5d134350f9435a3d5d
Daniel-Sogbey <mathematics06physics@gmail.com>
2022-01-28 13:01:23 +0000

    added post route

--- app.js
+++ app.js
@@ -1,14 +1,23 @@
 const path = require("path");
 const express = require("express");
 const pug = require("pug");
+const bodyParser = require("body-parser");
 
 const app = express();
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.set("view engine", pug);
 app.set("views", "views");
 app.use(express.static(path.join(__dirname, "public/")));
 
 app.get("/", (req, res) => {
+	console.log(req.body);
+	res.render(`index.pug`);
+});
+
+app.post("/", (req, res) => {
+	console.log(req.body);
 	res.render(`index.pug`);
 });
 
